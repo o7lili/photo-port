e61fc7c66e5ee82335abb5654c257c177414f6a3
Liliana Perez <103470899+o7lili@users.noreply.github.com>
2022-10-14 01:14:42 -0400

    control state w/ controlled component. need to fix formState not defined error

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+const { name, email, message } = formState;
 
 function ContactForm() {
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+
+    function handleChange(e) {
+        setFormState({...formState, [e.target.name]: e.target.value })
+    }
+
+    function handleSubmit(e) {
+        email.preventDefault();
+        console.log(formState);
+    }
+
     return (
         <section>
             <h1>Contact me</h1>
-            <form id="contact-form">
+            <form id="contact-form" onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" />
+                    <input type="text" defaultValue={name} onChange={handleChange} name="name" />
                 </div>
                 <div>
                     <label htmlFor="email">Email address:</label>
-                    <input type="email" name="email" />
+                    <input type="email" name="email" defaultValue={email} onChange={handleChange} />
                 </div>
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <textarea name="message" rows="5" />
+                    <textarea name="message" defaultValue={message} onChange={handleChange} rows="5" />
                 </div>
                 <button type="submit">Submit</button>
             </form>
